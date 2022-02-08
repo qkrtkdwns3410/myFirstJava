0009f0e3041d9fb795bddac862cd24d7ac7c724d
letscombine <qkrtkdwns3410@naver.com>
2022-02-08 17:42:23 +0900

    Changes

--- src/test/exercise/testmeplz2/programmers/GymSuit.java
+++ src/test/exercise/testmeplz2/programmers/GymSuit.java
@@ -1,6 +1,9 @@
 package test.exercise.testmeplz2.programmers;
 
+import javax.swing.text.html.ListView;
 import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 /**
@@ -18,7 +21,7 @@ import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
 
 public class GymSuit {
       public static void main(String[] args) {
-            solution(5, new int[]{2, 4}, new int[]{1, 3, 5});
+            solution(2, new int[]{1}, new int[]{2});
             System.out.println("======================================================");
             solution(5, new int[]{2, 4}, new int[]{3});
             System.out.println("======================================================");
@@ -27,8 +30,59 @@ public class GymSuit {
       }
 
       public static int solution(int n, int[] lost, int[] reserve) {
+            System.out.println("n = " + n);
+            System.out.println("reserve = " + Arrays.toString(reserve));
+            System.out.println("lost = " + Arrays.toString(lost));
+            System.out.println("======================================================");
+
             int answer = 0;
 
+            ArrayList<Integer> lostList = new ArrayList<>();
+            ArrayList<Integer> reserveList = new ArrayList<>();
+
+            for (int k : lost) {
+                  lostList.add(k);
+            }
+            for (int j : reserve) {
+                  reserveList.add(j);
+            }
+
+            int count = 0;
+
+            for (int lostIndex = 0; lostIndex < lostList.size(); lostIndex++) {
+                  int lostListValue = lostList.get(lostIndex);
+                  for (int reserveIndex = 0; reserveIndex < reserveList.size(); reserveIndex++) {
+                        int reserveListValue = reserveList.get(reserveIndex);
+
+
+                        if ((lostListValue < n && lostListValue > 1) && (reserveListValue == lostListValue - 1 || reserveListValue == lostListValue + 1)) {// 해당 값을 n 보다 작고 1보다 커야 양옆이 자유로움.
+                              System.out.println("lostListValue = " + lostListValue);
+                              System.out.println("reserveListValue = " + reserveListValue);
+                              System.out.println("양옆이 자유로운 친구들");
+                              reserveList.remove(reserveIndex);
+                              count++;
+
+                              break;
+                        } else if (lostListValue == n && reserveListValue == lostListValue - 1) {
+                              reserveList.remove(reserveIndex);
+                              count++;
+                              break;
+
+                        } else if (lostListValue == 1 && reserveListValue == lostListValue + 1) {
+                              reserveList.remove(reserveIndex);
+                              count++;
+                              break;
+
+                        }
+
+                  }
+            }
+            System.out.println("count = " + count);
+            System.out.println("lostList = " + lostList);
+            System.out.println("reserveList = " + reserveList);
+
+            answer = count + (n - lostList.size());
+            System.out.println("answer = " + answer);
             return answer;
       }
 }
