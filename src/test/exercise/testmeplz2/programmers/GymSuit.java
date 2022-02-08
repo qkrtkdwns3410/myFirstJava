a532b9e970b678670fe72c6677d561078f70ae2c
letscombine <qkrtkdwns3410@naver.com>
2022-02-08 19:42:04 +0900

    Changes

--- src/test/exercise/testmeplz2/programmers/GymSuit.java
+++ src/test/exercise/testmeplz2/programmers/GymSuit.java
@@ -19,14 +19,19 @@ import java.util.Arrays;
 
 public class GymSuit {
       public static void main(String[] args) {
-            solution(5, new int[]{2,4}, new int[]{3});
+            solution(5, new int[]{3, 5}, new int[]{2, 4});
             System.out.println("======================================================");
 
-            solution(5, new int[]{2,4}, new int[]{1,3,5});
+            solution(5, new int[]{1, 2}, new int[]{2, 3});
             System.out.println("======================================================");
 
-            solution(5, new int[]{2,3}, new int[]{3,4});
+            solution(10, new int[]{5, 4, 3, 2, 1}, new int[]{3, 1, 2, 5, 4});
 
+            System.out.println("======================================================");
+            solution(5, new int[]{2, 4}, new int[]{3});
+
+            System.out.println("======================================================");
+            solution(5, new int[]{2, 4}, new int[]{1, 3, 5});
       }
 
       public static int solution(int n, int[] lost, int[] reserve) {
@@ -34,6 +39,7 @@ public class GymSuit {
             System.out.println("reserve = " + Arrays.toString(reserve));
             System.out.println("lost = " + Arrays.toString(lost));
             System.out.println("======================================================");
+            int specialSum = 0;
 
             int answer = 0;
 
@@ -43,13 +49,13 @@ public class GymSuit {
             for (int k : lost) {
                   lostList.add(k);
             }
-
             for (int j : reserve) {
                   reserveList.add(j);
             }
+            specialSum = lostList.size();
 
-            for (int index = 0; index < lostList.size()-1; index++) {
-                  for (int index2 = 1; index2 < lostList.size(); index2++) {
+            for (int index = 0; index < lostList.size() - 1; index++) {
+                  for (int index2 = index; index2 < lostList.size(); index2++) {
                         if (lostList.get(index) > lostList.get(index2)) {
                               int temp = lostList.get(index);
                               int temp2 = lostList.get(index2);
@@ -59,8 +65,8 @@ public class GymSuit {
                   }
             }
 
-            for (int index = 0; index < reserveList.size()-1; index++) {
-                  for (int index2 = 1; index2 < reserveList.size(); index2++) {
+            for (int index = 0; index < reserveList.size() - 1; index++) {
+                  for (int index2 = index; index2 < reserveList.size(); index2++) {
                         if (reserveList.get(index) > reserveList.get(index2)) {
                               int temp = reserveList.get(index);
                               int temp2 = reserveList.get(index2);
@@ -70,21 +76,39 @@ public class GymSuit {
                         }
                   }
             }
+            System.out.println("lostList = " + lostList);
+            System.out.println("reserveList = " + reserveList);
 
             int count = 0;
-            for (int lostListValue : lostList) {
-                  for (int reserveIndex = 0; reserveIndex < reserveList.size(); reserveIndex++) {
-                        int reserveListValue = reserveList.get(reserveIndex);
+            int whileCount = 50;
+            while (whileCount > 0) {
+                  whileCount--;
+                  for (int lostIndex = 0; lostIndex < lostList.size(); lostIndex++) {
+                        int lostListValue = lostList.get(lostIndex);
 
-                        if (lostListValue == reserveListValue) {
-                              System.out.println("체육복 여분을 가진 친구가 또 잃어버림;;");
-                              reserveList.remove(reserveIndex);
-                              System.out.println("reserveList = " + reserveList);
-                              break;
+                        for (int reserveIndex = 0; reserveIndex < reserveList.size(); reserveIndex++) {
+                              int reserveListValue = reserveList.get(reserveIndex);
+
+                              if (lostListValue == reserveListValue) {
+
+                                    System.out.println("체육복 여분을 가진 친구가 또 잃어버림;;");
+                                    count++;
+                                    reserveList.remove(reserveIndex);
+                                    lostList.remove(lostIndex);
+
+                                    System.out.println("reserveList = " + reserveList);
+                                    System.out.println("lostList = " + lostList);
+
+                                    break;
+
+                              }
                         }
                   }
             }
-            for (int lostListValue : lostList) {
+
+
+            for (int i = 0; i < lostList.size(); i++) {
+                  int lostListValue = lostList.get(i);
                   for (int reserveIndex = 0; reserveIndex < reserveList.size(); reserveIndex++) {
                         int reserveListValue = reserveList.get(reserveIndex);
 
@@ -93,16 +117,22 @@ public class GymSuit {
                               System.out.println("reserveListValue = " + reserveListValue);
                               System.out.println("양옆이 자유로운 친구들");
                               reserveList.remove(reserveIndex);
+                              lostList.remove(i);
                               count++;
+
 
                               break;
                         } else if (lostListValue == n && reserveListValue == lostListValue - 1) {
                               reserveList.remove(reserveIndex);
+                              lostList.remove(i);
+
                               count++;
                               break;
 
                         } else if (lostListValue == 1 && reserveListValue == lostListValue + 1) {
                               reserveList.remove(reserveIndex);
+                              lostList.remove(i);
+
                               count++;
                               break;
 
@@ -114,7 +144,7 @@ public class GymSuit {
             System.out.println("lostList = " + lostList);
             System.out.println("reserveList = " + reserveList);
 
-            answer = count + (n - lostList.size());
+            answer = count + (n - specialSum) + lostList.size();
 
             System.out.println("answer = " + answer);
 
