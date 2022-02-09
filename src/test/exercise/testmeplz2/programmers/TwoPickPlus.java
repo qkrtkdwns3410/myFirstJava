bb8406d0c834a2f3af5d50175dc524fbf295eb62
letscombine <qkrtkdwns3410@naver.com>
2022-02-09 17:50:22 +0900

    Changes

---
+++ src/test/exercise/testmeplz2/programmers/TwoPickPlus.java
@@ -0,0 +1,68 @@
+package test.exercise.testmeplz2.programmers;
+
+import java.util.Arrays;
+
+
+/**
+ * packageName    : test.exercise.testmeplz2.programmers
+ * fileName       : TwoPickPlus
+ * author         : letscombine
+ * date           : 2022-02-09
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-02-09        letscombine       최초 생성
+ */
+
+
+public class TwoPickPlus {
+      
+      public static void main(String[] args) {
+      
+            solution(new int[]{2, 1, 3, 4, 1});
+            System.out.println("======================================================");
+            solution(new int[]{5, 0, 2, 7});
+            
+      }
+      
+      public static int[] solution(int[] numbers) {
+            int[] answer = {};
+      
+            System.out.println("numbers = " + Arrays.toString(numbers));
+            System.out.println("======================================================");
+            
+            
+            
+            
+            
+            return answer;
+      }
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
