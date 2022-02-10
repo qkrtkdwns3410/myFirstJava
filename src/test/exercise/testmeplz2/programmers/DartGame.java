02d68aba9187afd7dc2fb47b039b4f04d9ba3da6
letscombine <qkrtkdwns3410@naver.com>
2022-02-10 15:09:59 +0900

    Changes

---
+++ src/test/exercise/testmeplz2/programmers/DartGame.java
@@ -0,0 +1,125 @@
+package test.exercise.testmeplz2.programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+
+/**
+ * packageName    : test.exercise.testmeplz2.programmers
+ * fileName       : DartGame
+ * author         : letscombine
+ * date           : 2022-02-10
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-02-10        letscombine       최초 생성
+ */
+
+
+public class DartGame {
+      
+      public static void main(String[] args) {
+            
+            solution("1S2D*3T");
+            System.out.println("======================================================");
+            solution("1D2S#10S");
+            System.out.println("======================================================");
+            solution("1D2S0T");
+            System.out.println("======================================================");
+            solution("1S*2T*3S");
+            System.out.println("======================================================");
+            solution("1D#2S*3S");
+            System.out.println("======================================================");
+            solution("1T2D3D#");
+            System.out.println("======================================================");
+            solution("1D2S3T*");
+            
+      }
+      
+      public static int solution(String dartResult) {
+            
+            HashMap<String, String> optionMap = new HashMap<>();
+            optionMap.put("S", "1");
+            optionMap.put("D", "2");
+            optionMap.put("T", "3");
+            optionMap.put("#", "-");
+            
+            System.out.println("dartResult = " + dartResult);
+            ArrayList<String> calcList = new ArrayList<>();
+            String[] strArr;
+            
+            strArr = dartResult.split("");
+            System.out.println("strArr = " + Arrays.toString(strArr));
+      
+            for (int count = 0; count < strArr.length; count += 1) {
+                  System.out.println("strArr = " + strArr[count]);
+                  if (in(strArr[count], "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10")) {
+                        System.out.println("같은 값이 있다..");
+                        System.out.println("그리고 숫자형입니다");
+                  
+                  }
+            }
+            
+            return 0;
+      }
+      
+      public static double pow(double num1, int num2) {
+            
+            System.out.println("num1 = " + num1);
+            System.out.println("num2 = " + num2);
+            
+            double result = 0.0;
+            
+            for (int count = 0; count < num2; count += 1) {
+                  result *= num1;
+            }
+            
+            System.out.println("result = " + result);
+            
+            return result;
+            
+      }
+      
+      public static boolean in(String value,String... values) {
+      
+            System.out.println("values = " + Arrays.toString(values));
+            for (String v : values) {
+                  System.out.println("v = " + v);
+                  if (v.equals(value)) {
+                        return true;
+                  }
+            }
+            return false;
+            
+      }
+      
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
