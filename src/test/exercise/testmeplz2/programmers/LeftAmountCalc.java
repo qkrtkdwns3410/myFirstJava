d8a53961395c399bf881f83982340b7d147a7087
letscombine <qkrtkdwns3410@naver.com>
2022-02-10 11:14:04 +0900

    Changes

---
+++ src/test/exercise/testmeplz2/programmers/LeftAmountCalc.java
@@ -0,0 +1,75 @@
+package test.exercise.testmeplz2.programmers;
+
+/**
+ * packageName    : test.exercise.testmeplz2.programmers
+ * fileName       : LeftAmountCalc
+ * author         : letscombine
+ * date           : 2022-02-10
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-02-10        letscombine       최초 생성
+ */
+
+
+public class LeftAmountCalc {
+      
+      public static void main(String[] args) {
+      
+            solution(3, 20, 4);
+            System.out.println("======================================================");
+      }
+      
+      public static long solution(int price, int money, int count) {
+      
+            System.out.println("price = " + price);
+            System.out.println("money = " + money);
+            System.out.println("count = " + count);
+            System.out.println("======================================================");
+      
+            int priceFirst = price;
+            long answer = -1;
+      
+            int totalPrice = 0;
+            for (int repeat = 0; repeat < count; repeat += 1) {
+                  totalPrice += price;
+                  price +=priceFirst;
+                  
+            }
+            System.out.println("totalPrice = " + totalPrice);
+            int diff = totalPrice - money;
+            answer = diff >= 0 ? diff : 0;
+      
+            System.out.println("answer = " + answer);
+            return answer;
+      }
+      
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ src/test/exercise/testmeplz2/programmers/NamurziOne.java
@@ -0,0 +1,65 @@
+package test.exercise.testmeplz2.programmers;
+
+/**
+ * packageName    : test.exercise.testmeplz2.programmers
+ * fileName       : NamurziOne
+ * author         : letscombine
+ * date           : 2022-02-10
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-02-10        letscombine       최초 생성
+ */
+
+
+public class NamurziOne {
+      
+      public static void main(String[] args) {
+      
+            solution(10);
+            System.out.println("======================================================");
+            solution(12);
+            
+      }
+      
+      public static int solution(int n) {
+      
+            int x;
+            for (x = 1; x < n; x++) {
+                  if (n % x == 1) {
+                        return x;
+                  }
+            }
+      
+            return x;
+      }
+      
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
